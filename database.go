d968df232e38578012029a8a100d4afc52f9b0fb
RahulKraken <r.rahul4511@gmail.com>
2019-09-01 02:32:26 +0530

    getUser function issue resolved

--- database.go
+++ database.go
@@ -22,7 +22,7 @@ var db *sql.DB
 
 // CRUD application methods
 // create user
-func createUser(user User) {
+func createUser(db *sql.DB, user User) {
 	insert, err := db.Query("INSERT INTO user VALUES(?, ?)", user.ID, user.UserName)
 	if err != nil {
 		panic(err.Error())
@@ -31,24 +31,26 @@ func createUser(user User) {
 }
 
 // get user using id
-func getUser(id int) User {
+func getUser(db *sql.DB, id int) User {
 	data, err := db.Query("SELECT * FROM user WHERE id = ? LIMIT 1", id)
 	if err != nil {
 		panic(err.Error())
 	}
 
 	var user User
-	err = data.Scan(&user.ID, &user.UserName)
-	if err != nil {
-		panic(err.Error())
+	for data.Next() {
+		err = data.Scan(&user.ID, &user.UserName)
+		if err != nil {
+			panic(err.Error())
+		}
 	}
 
 	return user
 }
 
 // update user using id
-func updateUser(user User) User {
-	_, err := db.Query("UPDATE user WHERE id = ? SET user.user_name = ?", user.ID, user.UserName)
+func updateUser(db *sql.DB, user User) User {
+	_, err := db.Query("UPDATE user SET user.user_name = ? WHERE id = ?", user.UserName, user.ID)
 	if err != nil {
 		panic(err.Error())
 	}
@@ -57,7 +59,7 @@ func updateUser(user User) User {
 }
 
 // delete user using id
-func deleteUser(id int) {
+func deleteUser(db *sql.DB, id int) {
 	_, err := db.Query("DELETE FROM user WHERE id = ?", id)
 	if err != nil {
 		panic(err.Error())
