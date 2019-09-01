5177ae17f65043a8d0de2f2e2f770933dd9334e7
RahulKraken <r.rahul4511@gmail.com>
2019-09-01 14:04:22 +0530

    db obj nil error resolved

--- app/main.go
+++ app/main.go
@@ -3,9 +3,26 @@ package main
 import (
 	"fmt"
 	"github.com/RahulKraken/Paste-it/database"
+	_ "github.com/go-sql-driver/mysql"
+	"database/sql"
 )
+
+// database connection
+var db *sql.DB
 
 func main() {
 	fmt.Println("Hello from main!!!")
-	database.CreateUser()
+	// try connecting to database
+	db, err := sql.Open("mysql", "pasteit:pasteit@tcp(127.0.0.1:3306)/pasteit_db")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println("testing db here ->", db)
+	database.CreateUser(db, database.User{ID: 2, UserName: "Kraken"})
+	// user := database.User{
+	// 	ID: 2,
+	// 	UserName: "Kraken",
+	// }
+	// database.CreateUser(db, user)
 }
--- database/database.go
+++ database/database.go
@@ -1,7 +1,6 @@
 package database
 
 import (
-	"fmt"
 	"database/sql"
 	// import and use as driver
 	_ "github.com/go-sql-driver/mysql"
@@ -13,13 +12,10 @@ type User struct {
 	UserName string `json:"user_name"`
 }
 
-// db connection var
-var db *sql.DB
-
 // CRUD application methods for user table
 
-// createUser - create new user
-func createUser(db *sql.DB, user User) {
+// CreateUser - create new user
+func CreateUser(db *sql.DB, user User) {
 	insert, err := db.Query("INSERT INTO user VALUES(?, ?)", user.ID, user.UserName)
 	if err != nil {
 		panic(err.Error())
@@ -27,8 +23,8 @@ func createUser(db *sql.DB, user User) {
 	insert.Close()
 }
 
-// getUser - get user with id
-func getUser(db *sql.DB, id int) User {
+// GetUser - get user with id
+func GetUser(db *sql.DB, id int) User {
 	data, err := db.Query("SELECT * FROM user WHERE id = ? LIMIT 1", id)
 	if err != nil {
 		panic(err.Error())
@@ -45,8 +41,8 @@ func getUser(db *sql.DB, id int) User {
 	return user
 }
 
-// updateUser - update user
-func updateUser(db *sql.DB, user User) User {
+// UpdateUser - update user
+func UpdateUser(db *sql.DB, user User) User {
 	_, err := db.Query("UPDATE user SET user.user_name = ? WHERE id = ?", user.UserName, user.ID)
 	if err != nil {
 		panic(err.Error())
@@ -55,8 +51,8 @@ func updateUser(db *sql.DB, user User) User {
 	return user
 }
 
-// deleteUser - delete user with id
-func deleteUser(db *sql.DB, id int) {
+// DeleteUser - delete user with id
+func DeleteUser(db *sql.DB, id int) {
 	_, err := db.Query("DELETE FROM user WHERE id = ?", id)
 	if err != nil {
 		panic(err.Error())
@@ -73,8 +69,8 @@ type Paste struct {
 
 // CRUD application methods for paste table
 
-// createPaste - create new paste
-func createPaste(db *sql.DB, paste Paste) {
+// CreatePaste - create new paste
+func CreatePaste(db *sql.DB, paste Paste) {
 	insert, err := db.Query("INSERT INTO paste VALUES (?, ?, ?, ?)", paste.ID, paste.UserID, paste.Title, paste.Content)
 	if err != nil {
 		panic(err.Error())
@@ -82,8 +78,8 @@ func createPaste(db *sql.DB, paste Paste) {
 	insert.Close()
 }
 
-// updatePaste - update existing paste
-func updatePaste(db *sql.DB, paste Paste) Paste {
+// UpdatePaste - update existing paste
+func UpdatePaste(db *sql.DB, paste Paste) Paste {
 	_, err := db.Query("UPDATE paste SET title = ?, content = ? WHERE id = ? AND user_id = ?", paste.Title, paste.Content, paste.ID, paste.UserID)
 	if err != nil {
 		panic(err.Error())
@@ -92,8 +88,8 @@ func updatePaste(db *sql.DB, paste Paste) Paste {
 	return paste
 }
 
-// getPaste - get paste with id
-func getPaste(db *sql.DB, id int) Paste {
+// GetPaste - get paste with id
+func GetPaste(db *sql.DB, id int) Paste {
 	val, err := db.Query("SELECT * FROM paste WHERE id = ?", id)
 	if err != nil {
 		panic(err.Error())
@@ -110,70 +106,10 @@ func getPaste(db *sql.DB, id int) Paste {
 	return paste
 }
 
-// deletePaste - delete paste with id
-func deletePaste(db *sql.DB, id int) {
+// DeletePaste - delete paste with id
+func DeletePaste(db *sql.DB, id int) {
 	_, err := db.Query("DELETE FROM paste WHERE id = ?", id)
 	if err != nil {
 		panic(err.Error())
 	}
-}
-
-// exposed CRUD functions
-// User functions
-
-// CreateUser - new user
-func CreateUser(user User) {
-	createUser(db, user)
-}
-
-// UpdateUser - update existing user
-func UpdateUser(user User) {
-	updateUser(db, user)
-}
-
-// GetUser - get user with id
-func GetUser(id int) User {
-	return getUser(db, id)
-}
-
-// DeleteUser - delete user with id
-func DeleteUser(id int) {
-	deleteUser(db, id)
-}
-
-// Paste functions
-
-// CreatePaste - create new paste
-func CreatePaste(paste Paste) {
-	createPaste(db, paste)
-}
-
-// UpdatePaste - update paste
-func UpdatePaste(paste Paste) {
-	updatePaste(db, paste)
-}
-
-// GetPaste - get paste with id
-func GetPaste(id int) Paste {
-	return getPaste(db, id)
-}
-
-// DeletePaste - delete paste with id
-func DeletePaste(id int) {
-	deletePaste(db, id)
-}
-
-func init() {
-	// try connecting to database
-	db, err := sql.Open("mysql", "pasteit:pasteit@tcp(127.0.0.1:3306)/pasteit_db")
-	if err != nil {
-		panic(err.Error())
-	}
-
-	// trying to ping database
-	err = db.Ping();
-	if err != nil {
-		panic(err.Error())
-	}
-	fmt.Println("database connection successfull!!!")
 }
